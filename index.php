0ec8908f8253533d1a6115e9b96e0180ebf24d57
renosz <32728131+renosz@users.noreply.github.com>
2018-07-07 13:41:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -137,16 +137,17 @@ array (
 							'replyToken' => $replyToken,														
 							'messages' => array(
 array (
-  'type' => 'image',
-  'originalContentUrl' => 'https://stickershop.line-scdn.net/stickershop/v1/sticker/16510674/ANDROID/sticker.png;compress=true',
-  'previewImageUrl' => 'https://stickershop.line-scdn.net/stickershop/v1/sticker/16510674/ANDROID/sticker.png;compress=true',
-  'animated' => false,
-	array (
-	  'type' => 'image',
-	  'originalContentUrl' => 'https://stickershop.line-scdn.net/stickershop/v1/sticker/16510675/ANDROID/sticker.png;compress=true',
-	  'previewImageUrl' => 'https://stickershop.line-scdn.net/stickershop/v1/sticker/16510675/ANDROID/sticker.png;compress=true',
-	  'animated' => false,
-	)
+  0 =>
+  	'type' => 'image',
+  	'originalContentUrl' => 'https://stickershop.line-scdn.net/stickershop/v1/sticker/16510674/ANDROID/sticker.png;compress=true',
+  	'previewImageUrl' => 'https://stickershop.line-scdn.net/stickershop/v1/sticker/16510674/ANDROID/sticker.png;compress=true',
+  	'animated' => false,
+  1 =>
+	'type' => 'image',
+	'originalContentUrl' => 'https://stickershop.line-scdn.net/stickershop/v1/sticker/16510675/ANDROID/sticker.png;compress=true',
+	'previewImageUrl' => 'https://stickershop.line-scdn.net/stickershop/v1/sticker/16510675/ANDROID/sticker.png;compress=true',
+	'animated' => false,
+)
 							)
 						);
 				
