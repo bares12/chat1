1c76d3ce402c269cdc04602966f58de6e4eba748
renosz <32728131+renosz@users.noreply.github.com>
2018-07-08 02:51:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,10 +14,8 @@ $timestamp	= $client->parseEvents()[0]['timestamp'];
 $message 	= $client->parseEvents()[0]['message'];
 $messageid 	= $client->parseEvents()[0]['message']['id'];
 $pesan_datang = $message['text'];
-$response = $client->profile($userId);
-if($response->isSucceeded())
-	$profile = $response->getJSONDecodedBody();
-	$displayName = $profile['displayName']
+$profile = $client->profile($userId);
+$displayName = $profile['displayName']
 //pesan bergambar
 
 if($type=='join')
