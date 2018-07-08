1d9fc3b593022bcfb1692e0c7767ada980b6a987
renosz <32728131+renosz@users.noreply.github.com>
2018-07-09 00:44:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,6 +15,8 @@ $message 	= $client->parseEvents()[0]['message'];
 $messageid 	= $client->parseEvents()[0]['message']['id'];
 $pesan_datang = strtolower($message['text']);
 $profil = $client->profil($userId);
+$profile = $profil->getJSONDecodedBody();
+$displayname = $profile['displayName']
 $textsplit = explode("|", $message['text']);
 
 $command = $textsplit[0];
@@ -145,7 +147,7 @@ array (
 							'messages' => array(
 array (
   'type' => 'text',
-  'text' => 'YourName @'.$profil->statusMessage.' '
+  'text' => $displayname
 )
 							)
 						);
